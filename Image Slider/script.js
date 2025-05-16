94a601fc9b8d081538b867965c031ab6ebbb4eb3
aminszo <amin.salehizade@gmail.com>
2025-05-16 23:04:46 +0330

    Update Image Slider

--- Image Slider/script.js
+++ Image Slider/script.js
@@ -1,25 +1,30 @@
-const images = document.getElementById('image-container');
+const imageContainer = document.getElementById('image-container');
+const images = document.querySelectorAll('#image-container img')
 const prevBtn = document.getElementById('prev-slide');
 const nextBtn = document.getElementById('next-slide');
 
 let idx = 0;
 
-function changeSlide() {
+function changeSlide(direction = 1) {
+
+    idx += direction;
+
     if (idx > images.length - 1) {
         idx = 0;
     } if (idx < 0) {
         idx = images.length - 1;
     }
 
-    images.style.transform = `translateX(${-idx * 500}px)`;
+    imageContainer.style.transform = `translateX(${-idx * 500}px)`;
 }
 
-prevBtn.addEventListener('click', () => {
-    idx--;
-    changeSlide();
-});
+prevBtn.addEventListener('click', () => changeSlide(-1));
+nextBtn.addEventListener('click', () => changeSlide(1));
 
-nextBtn.addEventListener('click', () => {
-    idx++;
-    changeSlide();
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
+        changeSlide(1);
+    } else if (e.key === 'ArrowLeft') {
+        changeSlide(-1);
+    }
 });
